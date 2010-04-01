8515f15013a50dd84d66ea1874ac2f0ec1a9e5b0
unknown <danny@.(none)>
2010-04-01 21:58:20 +0200

    start of application

---
+++ mario/MarioWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mario;
+
+/**
+ *
+ * @author danny
+ */
+class MarioWorld
+{
+    public MarioWorld()
+    {
+    }
+}
