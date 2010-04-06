bd8c2b319c030818ce7c134c6f18b5818ba1024d
Danny <DeDanny@gmail.com>
2010-04-06 13:32:12 +0200

    Merge problem

---
+++ mario/state/bigMario.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mario.state;
+
+import mario.Mario;
+
+/**
+ *
+ * @author Danny
+ */
+public class bigMario extends MoveState{
+
+    public bigMario(Mario gameObject)
+    {
+        super(gameObject);
+    }
+}
