91138da08ef46e7ec3d73712ccca2958c4abda06
Danny <DeDanny@gmail.com>
2010-04-06 13:34:12 +0200

    Merge problem bigMario

--- mario/state/bigMario.java
+++ mario/state/bigMario.java
@@ -7,14 +7,53 @@ package mario.state;
 
 import mario.Mario;
 
+
 /**
  *
- * @author Danny
+ * @author Onno
  */
-public class bigMario extends MoveState{
+public class BigMario extends MoveState
+{
+    protected static String[] RightAnimation =
+    {
+        "bigMarioWalkRight 0", "bigMarioWalkRight 1"
+    };
+    protected  static String[] LeftAnimation =
+    {
+        "bigMarioWalkLeft 0", "bigMarioWalkLeft 1"
+    };
+    protected  static String[] StandLeft =
+    {
+        "bigMarioStandLeft 0"
+    };
+    protected  static String[] StandRight =
+    {
+        "bigMarioStandRight 0"
+    };
+    protected  static String[] UpRight =
+    {
+        "bigMarioLookUpRight 0"
+    };
+    protected  static String[] UpLeft =
+    {
+        "bigMarioLookUpLeft 0"
+    };
+    protected  static String[] DuckRight =
+    {
+        "bigMarioDuckRight 0"
+    };
+    protected  static String[] DuckLeft =
+    {
+        "bigMarioDuckLeft 0"
+    };
 
-    public bigMario(Mario gameObject)
+    protected int       duckHeight      = 3;
+    protected int       lookupHeight    = 3;
+
+    public BigMario(Mario gameObject)
     {
         super(gameObject);
     }
+
+
 }
