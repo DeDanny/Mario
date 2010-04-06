5bae92c35c1806de4b49aa300e04f19bd2143715
unknown <Danny@.(none)>
2010-04-06 12:05:51 +0200

    added enemy

---
+++ mario/enemy/Enemy.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mario.enemy;
+
+import mario.GameObject;
+
+/**
+ *
+ * @author Danny
+ */
+abstract public class Enemy extends GameObject
+{
+
+    public Enemy(int x, int y, int width, int height, String fileName)
+    {
+
+        super(x, y, width, height, fileName);
+    }
+    
+    @Override
+    public abstract void doLoopAction();
+
+    public abstract void hitBy();
+}
