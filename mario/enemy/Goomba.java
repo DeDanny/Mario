9b3a46e1c49bb04b8f7218dd005e47a1cc83d4c7
Danny <DeDanny@gmail.com>
2010-04-07 14:14:44 +0200

    Goomba now walks of the screen

--- mario/enemy/Goomba.java
+++ mario/enemy/Goomba.java
@@ -14,16 +14,15 @@ import mario.enemy.ai.GoombaAi;
 public class Goomba extends Enemy
 {
 
-    public Goomba()
+    public Goomba(int x, int y, int width, int height)
     {
-        super(300, 100, 66, 66, "/images/smw_enemies_sheet.png");
+        super(x, y, width, height, "/images/smw_enemies_sheet.png");
         ai = new GoombaAi(this);
         frames.put("goombaStandLeft 0", new Rectangle(156, 978, 48, 48));
         frames.put("goombaWalkLeft 0", new Rectangle(35, 978, 50, 48));
         setAnimation(new String[]
                 {
                     "goombaStandLeft 0", "goombaWalkLeft 0"
-
         });
     }
 
