80057870d62e9f7489cf076bcdf353e69d309c90
Nishchal Baldew <n.baldew@hotmail.com>
2010-04-09 13:31:54 +0200

    mario cube

--- mario/Game.java
+++ mario/Game.java
@@ -4,16 +4,10 @@
  */
 package mario;
 
-<<<<<<< HEAD
-import background.Background;
-import blokjes.Munt;
-import blokjes.Questionmark;
-import blokjes.Stone;
-=======
 import mario.background.Background;
+import mario.cube.Munt;
 import mario.cube.Questionmark;
 import mario.cube.Stone;
->>>>>>> 1de9164851077f9c1b8823bfba8fcc8b07f3f6bf
 import java.util.ArrayList;
 import mario.enemy.*;
 
@@ -23,26 +17,16 @@ import mario.enemy.*;
  */
 public class Game
 {
-<<<<<<< HEAD
     private boolean running         = false;
     private boolean paused          = false;
 
 
-    private Mario mario = new Mario();
-    private Goomba goomba = new Goomba(300, 512, 66, 66);
-    private Background background   = new Background();
-    private Munt Munt   = new Munt();
-    private Questionmark Questionmark   = new Questionmark();
-    private Stone Stone   = new Stone();
-=======
-    private boolean running = false;
-    private boolean paused = false;
     private Mario mario = new Mario(this);
-    private Goomba goomba = new Goomba(this, 300, 450, 66, 66);
-    private Background background = new Background(this);
-    private Questionmark Questionmark = new Questionmark(this);
-    private Stone Stone = new Stone(this);
->>>>>>> 1de9164851077f9c1b8823bfba8fcc8b07f3f6bf
+    private Goomba goomba = new Goomba(this, 300, 512, 66, 66);
+    private Background background  = new Background(this);
+    private Munt Munt = new Munt(this);
+    private Questionmark Questionmark  = new Questionmark(this);
+    private Stone Stone  = new Stone(this);
     private ArrayList<GameObject> gameObjects = new ArrayList<GameObject>();
 
     public Game()
---
+++ mario/cube/Munt.java
@@ -0,0 +1,66 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mario.cube;
+
+import java.awt.Rectangle;
+import mario.*;
+
+
+/**
+ *
+ * @author Nishchal Baldew
+ */
+public class Munt extends GameObject {
+
+    private boolean raak = true;
+    private int goinUp = 0;
+
+    public Munt(Game game) {
+        super(game, 100, 350, 63, 63, "/images/nsmbtileset.png");
+
+        frames.put("munt 0", new Rectangle(457, 167, 45, 48));
+        
+        setAnimation(new String[]{"munt 0"});
+
+    }
+
+    @Override
+    public void doLoopAction() {
+        if(raak)
+        {
+            if(goinUp < 20)
+            {
+                setY(getY() - 5);
+                goinUp++;
+
+            }
+            else
+            {
+                setAnimation(new String[] {"munt 0"});
+                 if(goinUp < 4)
+                 {
+                raak = false;
+
+                 }
+            }
+        }
+        /*
+        if (y > 95) {
+            setY(getY() - 5);
+        } else if (raak = true) {
+            setY(getY() + 5);
+        }
+        raak = false;
+         * */
+    }
+
+    //@Override
+    //public void doGravity()
+    {
+
+    }
+}
+
+
