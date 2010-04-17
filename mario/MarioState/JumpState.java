50abb34c1f74c5e5cb8cb8c2aae5533cc3ad21d1
Danny <DeDanny@gmail.com>
2010-04-17 15:17:52 +0200

    jump does now interact with objects

--- mario/MarioState/JumpState.java
+++ mario/MarioState/JumpState.java
@@ -6,88 +6,136 @@ package mario.MarioState;
 
 import mario.core.Direction;
 import mario.Mario;
+import mario.core.Collision;
 
 /**
  *
  * @author danny
  */
-public class JumpState extends MarioState {
-
+public class JumpState extends MarioState
+{
     protected static final int WALKSPEED = 4;
     private int jumpTeller = 0;
-    protected String[] smallJumpRight = new String[]{"smallMarioJumpRight 0"};
-    protected String[] smallJumpLeft = new String[]{"smallMarioJumpLeft 0"};
-    protected String[] bigJumpRight = new String[]{"bigMarioJumpRight 0"};
-    protected String[] bigJumpLeft = new String[]{"bigMarioJumpLeft 0"};
+    protected String[] smallJumpRight = new String[]
+    {
+        "smallMarioJumpRight 0"
+    };
+    protected String[] smallJumpLeft = new String[]
+    {
+        "smallMarioJumpLeft 0"
+    };
+    protected String[] bigJumpRight = new String[]
+    {
+        "bigMarioJumpRight 0"
+    };
+    protected String[] bigJumpLeft = new String[]
+    {
+        "bigMarioJumpLeft 0"
+    };
     private int jumpHeight;
 
-    public JumpState(Mario marioObject) {
+    public JumpState(Mario marioObject)
+    {
         super(marioObject);
 
     }
 
     @Override
-    public void doAction() {
-        if (!marioObject.isPreformingSpecialMove()) {
-            if (marioObject.isLeft() && !marioObject.isRight()) {
+    public void doAction()
+    {
+        if (!marioObject.isPreformingSpecialMove())
+        {
+            if (marioObject.isLeft() && !marioObject.isRight())
+            {
                 doLeft();
                 doJumping();
-            } else if (marioObject.isRight() && !marioObject.isLeft()) {
+            }
+            else if (marioObject.isRight() && !marioObject.isLeft())
+            {
                 doRight();
                 doJumping();
-            } else if (marioObject.getDirection() == Direction.LEFT) {
+            }
+            else if (marioObject.getDirection() == Direction.LEFT)
+            {
                 setAnimationLeft();
                 doJumping();
-            } else if (marioObject.getDirection() == Direction.RIGHT) {
+            }
+            else if (marioObject.getDirection() == Direction.RIGHT)
+            {
                 setAnimationRight();
                 doJumping();
             }
-        } else {
+        }
+        else
+        {
             marioObject.setJump(false);
         }
     }
 
-    private void doLeft() {
+    private void doLeft()
+    {
         marioObject.setX(marioObject.getX() - WALKSPEED);
         setAnimationLeft();
         marioObject.setDirection(Direction.LEFT);
     }
 
-    private void doRight() {
+    private void doRight()
+    {
         marioObject.setX(marioObject.getX() + WALKSPEED);
         setAnimationRight();
         marioObject.setDirection(Direction.RIGHT);
     }
 
-    private void setAnimationLeft() {
-        if (marioObject.getIsBig()) {
+    private void setAnimationLeft()
+    {
+        if (marioObject.getIsBig())
+        {
             super.setAnimation(bigJumpLeft);
-        } else {
+        }
+        else
+        {
             super.setAnimation(smallJumpLeft);
         }
     }
 
-    private void setAnimationRight() {
-        if (marioObject.getIsBig()) {
+    private void setAnimationRight()
+    {
+        if (marioObject.getIsBig())
+        {
             super.setAnimation(bigJumpRight);
-        } else {
+        }
+        else
+        {
             super.setAnimation(smallJumpRight);
         }
     }
 
-    private void doJumping() {
-     
+    private void doJumping()
+    {
+
         jumpHeight = 10;
-        if (jumpTeller > 10) {
+        if (jumpTeller > 10)
+        {
             jumpHeight--;
         }
 
-        if (jumpTeller < 15) {
+        if (jumpTeller < 15)
+        {
+            while (marioObject.checkCollisionMap(marioObject.getX(), marioObject.getY() - jumpHeight) != Collision.NONE && jumpHeight != 0)
+            {
+                jumpHeight--;
+                System.out.println("jumpHeight = " + jumpHeight);
+            }
+
             marioObject.setY(marioObject.getY() - jumpHeight);
+
+
             jumpTeller++;
-            
+
             //System.out.println("teller " + jumpTeller);
-        } else {
+        }
+        else
+        {
             marioObject.setJump(false);
             marioObject.setFall(true);
             //System.out.println("set jump false yippykayeay");
